53a37fe2b7d4421eef03b8a09f0883352b797231
Akash Pawar <akashshivrampawar@gmail.com>
2024-10-02 22:19:04 -0700

    remove profile route

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://akashpawar:akashpawar@cluster0.1cuo7.mongodb.net/test')
+mongoose.connect("mongodb+srv://akashpawar:akashpawar@cluster0.1cuo7.mongodb.net/workManagerDatabse")
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Error connecting to MongoDB:', err));
 
